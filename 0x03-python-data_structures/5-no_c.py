5800d341101c761c5967b647f5b498850a2ab4f5
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-07 01:56:05 +0200

    correction1

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    a = len(my_string)
+    i = 0
+    while i < a:
+         if my_string[i] == 'c' or my_string[i] == 'C':
+            my_string[i]=[]
+         i = i + 1
+    return my_string
