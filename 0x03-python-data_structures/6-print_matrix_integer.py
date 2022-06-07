502f7f85d46e5ba8a2ff479a40366f02fde772a7
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-07 02:12:02 +0200

    correction1

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    a = len(matrix)
+    i = 0
+    while i < a:
+        j = 0
+        while j < a:
+            print("{}".format(matrix=[i][j])
+            j = j + 1
+        i = i + 1
+
