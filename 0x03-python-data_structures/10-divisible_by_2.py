4805e785bd8ab80af92b749e0ecda446e87e607a
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-07 04:28:07 +0200

    correction1

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    a = len (my_list)
+    if a == 0:
+        return None
+    else:
+        nlist = my_list.copy()
+        i = 0
+        while i < a:
+            if my_list[i] % 2 == 0:
+                nlist[i] = True
+            else:
+                nlist[i] = False
+            i = i + 1
+        return nlist
