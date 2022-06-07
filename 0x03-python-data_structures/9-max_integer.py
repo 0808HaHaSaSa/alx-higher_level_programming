ea2d43bd514d2048b36c48dde46712c6d3dcf93f
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-07 04:11:39 +0200

    correction1

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    a = len (my_list)
+    if a == 0:
+        return None
+    else:
+        i = 1
+        b = my_list[0]
+        while i < a:
+            if b < my_list[i]:
+                b = my_list[i]
+            i = i + 1
+        return b
