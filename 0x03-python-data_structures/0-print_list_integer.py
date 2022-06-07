cab5ab341f32b17f1cb441928bf20d7c5202e79d
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-07 03:38:58 +0200

    correction1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     a = len(my_list)
     i = 0
     while i < a:
-         print("{}".format(my_list[i]))
+         print("{:d}".format(my_list[i]))
          i = i + 1
