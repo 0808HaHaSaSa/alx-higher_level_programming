ef5f38bfe9864cd57ac7c77166a0f4c4d1ca7d87
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-09 21:47:53 +0200

    correction

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,13 +4,13 @@ def uniq_add(my_list=[]):
     i = 0
     m = 0
     while i < a:
-        l = 0
+        ma = 0
         j = 0
         while j < i:
             if my_list[i] == my_list[j]:
-                l = l + 1
+                ma = ma + 1
             j = j + 1
-        if l == 0:
+        if ma == 0:
             m = m + my_list[i]
         i = i + 1
     return m
