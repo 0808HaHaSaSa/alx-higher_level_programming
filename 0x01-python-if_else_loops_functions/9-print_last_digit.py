3a7adc16769160afe2d2e7640f2eadbe358cfb22
root <root@DESKTOP-VB97GVI.localdomain>
2022-07-06 17:06:42 +0100

    done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    last = number % 10
-    print('{:d}'.format(last), end='')
-    return last
+	if number >= 0:
+		mod = number % 10
+	else:
+		mod = -((-1024) % (-10))
+	print("{}".format(mod), end='')
+	return mod
