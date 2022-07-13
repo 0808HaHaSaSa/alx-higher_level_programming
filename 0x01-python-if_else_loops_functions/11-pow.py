f6bb1482887ceadceb5ab1913a85c909b439a90e
root <root@DESKTOP-VB97GVI.localdomain>
2022-07-13 15:29:33 +0100

    done

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-if b == 0:
-	return 1
-elif b > 0:
-	c = 1
-	for i in range(0, b):
-		c = c*a
-	return c
-else:
-	c = 1
-	for i in range(0, -b):
-		c = c*a
-	return 1/c
+	if b == 0:
+		return 1
+	elif b > 0:
+		c = 1
+		for i in range(0, b):
+			c = c*a
+		return c
+	else:
+		c = 1
+		for i in range(0, -b):
+			c = c*a
+		return 1/c
